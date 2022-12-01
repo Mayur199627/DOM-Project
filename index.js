9ffe0bb56313e74ca4be2b60873afe59b591fb5a
Mayur199627 <mayur27asati@gmail.com>
2022-12-01 14:23:53 +0530

    final

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const select = document.getElementById('firstHalf');
 const btn = document.getElementById('changeButton');
 const select2 = document.getElementById('secondHalf');
 const btn2 = document.getElementById('changeButton1');
+const select3 = document.getElementById('thirdHalf');
+const btn3 = document.getElementById('changeButton2');
 
 /* Direct Color From Array */
 const color = ['red','yellow','pink','gray','aqua','lime','orange','brown','black','green'];
@@ -19,4 +21,16 @@ btn.addEventListener('click',()=>{
 btn2.addEventListener('click',()=>{
 select2.style.backgroundColor = `rgb(${Math.floor(255*Math.random(0,1))},${Math.floor(255*Math.random(0,1))},${Math.floor(255*Math.random(0,1))})`
 btn2.textContent = `rgb(${Math.floor(255*Math.random())},${Math.floor(250*Math.random())},${Math.floor(255*Math.random())})`
+})
+
+/* Color of HEX value */
+btn3.addEventListener('click',()=>{
+    let colorHex ="#";
+    let value = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+    for(let i = 0; i<6; i++){
+        let index = Math.floor(Math.random()*value.length)
+        colorHex+=value[index]
+    }
+    select3.style.backgroundColor = colorHex;
+    btn3.textContent = colorHex;
 })
